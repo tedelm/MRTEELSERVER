bf08378fccb628077689ba0aa8a658f88a8846ea
Ted Elmenheim <ted elmenheim>
2018-11-09 13:40:24 +0100

    changes

--- www/index.php
+++ www/index.php
@@ -3,36 +3,135 @@ include_once("include/common_db.php");
 include_once("include/common_db_query.php");
 
 //Get all ispindles
-$q_sql_everything = mysql_query("SELECT * FROM Data ORDER BY Name DESC LIMIT 1;") or die(mysql_error());
+$q_sql_everything = mysql_query("SELECT * FROM Data ORDER BY Timestamp DESC LIMIT 1;") or die(mysql_error());
+while($r_everything = mysql_fetch_array($q_sql_everything))
+{
+  $timestamp = $r_everything['Timestamp'];
+  $Name =  $r_everything['Name'];
+  $ID =  $r_everything['ID'];
+  $Angle =  $r_everything['Angle'];            
+  $Temperature =  $r_everything['Temperature'];
+  $Battery =  $r_everything['Battery'];
+  $Gravity =  $r_everything['Gravity'];      
+  $RSSI =  $r_everything['RSSI'];                    
 
-    while($r_everything = mysql_fetch_array($q_sql_everything))
-    {
+}	
 
-        echo "</br> Timestamp: ";
-      echo $r_everything['Timestamp'];
-      echo "</br> Name: ";
-      echo $r_everything['Name'];
-      echo "</br> ID: ";
-      echo $r_everything['ID'];
-      echo "</br> Angle: ";
-      echo $r_everything['Angle'];            
-      echo "</br> Temperature: ";
-      echo $r_everything['Temperature'];
-      echo "</br> Battery: ";
-      echo $r_everything['Battery'];
-      echo "</br> Gravity: ";
-      echo $r_everything['Gravity'];      
-      echo "</br> RSSI: ";
-      echo $r_everything['RSSI'];                    
+//Calculate plato/SG
+$Plato = 0.004415613 * $Angle * $Angle + 0.120848707 * $Angle - 6.159197377;
+ #$SG = 1+($plato/ (258.6–(($plato/258.2)*227.1)))
+$Plato_1 = ($Plato / 258.2) * 227.1;
+$Plato_2 =  258.6 - $Plato_1;
+$Plato_3 =  $Plato/$Plato_2;
+$SG = 1 + $Plato_3;
 
-    }	
+#$recipename
+#$OG
+#$ABV
 
-	
 ?>
 <html>
 <head>
+<link rel="stylesheet" type="text/css" href="css/default.css">
+<style>
+@import url('https://fonts.googleapis.com/css?family=Audiowide');
+</style>
 </head>
-    <body>
-        Main site
-    </body>
+
+<body>
+<center>
+<table border='0'>
+    <tr>
+        <td>
+            <img src='img/mrteellogo.png' width='55' height='75'>
+        </td>
+        <td>
+            <div id="container">
+            <p>
+            <a href="index.php">MRTEELSERVER</a>
+            </p>
+            </div>
+        </td>
+    </tr>
+</table>
+<table border='0'>    
+    <tr>
+        <td>
+          <div class="cardMenu card-4"><a href='index.php'>Dashboard</a></div>
+        </td>
+        <td>
+          <div class="cardMenu card-4"><a href='devices.php'>Devices</a></div>            
+        </td>
+    </tr>
+</table>
+
+<div class="cardLongHeadline card-4">
+    iSpindel:
+</div>
+<div class="cardLongHeadline card-4">
+    recipe:
+</div>
+</br>
+<div class="cardLong card-4">
+    <?php echo $Name; ?>
+</div>
+<div class="cardLong card-4">
+    <?php if(isset($recipename)){
+        echo $recipename;
+        }else{
+            echo "N/A";
+        };
+    ?>    
+</div>
+</br>
+<div class="card card-small">
+    Temp</br>
+    <?php echo round($Temperature, 2); ?>
+</div>
+<div class="card card-small">
+    Tilt</br>
+    <?php echo round($Angle, 2); ?>
+</div>
+<div class="card card-small">
+    Battery</br>
+    <?php echo round($Battery, 2); ?>
+</div>
+<div class="card card-small">
+    Ispindel Plato</br>
+    <?php echo round($Gravity, 2); ?>
+</div>
+<div class="card card-small">
+    RSSI</br>
+    <?php echo round($RSSI, 0); ?>
+</div>
+</br>
+
+<div class="card card-small">
+    PLATO</br>
+    <?php echo round($Plato, 2); ?>
+</div>
+<div class="card card-small">
+    SG</br>
+    <?php echo round($SG, 3); ?>
+</div>
+<div class="card card-small">
+    OG</br>
+    <?php if(isset($OG)){
+        echo round($OG, 3);
+        }else{
+            echo "N/A";
+        };
+    ?>
+</div>
+<div class="card card-small">
+    ABV</br>
+    <?php if(isset($ABV)){
+        echo round($ABV, 3);
+        }else{
+            echo "N/A";
+        };
+    ?>
+</div>
+</center>
+</body>
 </html>
